3233e1950b18ddc31a42637f87ed3ffd6e368382
Jorge Gonçalves <u.jorgea@gmail.com>
2020-01-28 10:32:20 +0000

    added csrf token

--- app.js
+++ app.js
@@ -5,17 +5,27 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
 const flash = require('connect-flash');
 
+const mongoDB_URI = 'mongodb+srv://jorge:mongodb@cluster0-8c4e8.mongodb.net/Middleware';
+
 const app = express();
+const store = new MongoDBStore({
+    uri: mongoDB_URI,
+    collection: 'sessions'
+});
+
+const csrfProtection = csrf();
 
 const errorController = require('./controllers/error');
 const isAuth = require('./middleware/is-auth');
 
-const mongoDB_URI = 'mongodb+srv://jorge:mongodb@cluster0-8c4e8.mongodb.net/Middleware';
-
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+
+
 
 const authRoutes = require('./routes/auth');
 const erpRoutes = require('./routes/erp');
@@ -29,9 +39,16 @@ app.use(
     session({
         secret: 'my secret',
         resave: false,
-        saveUninitialized: false
+        saveUninitialized: false,
+        store: store
     })
 );
+
+app.use(csrfProtection);
+app.use((req, res, next) => {
+    res.locals.csrfToken = req.csrfToken();
+    next();
+});
 
 app.use(flash());
 
